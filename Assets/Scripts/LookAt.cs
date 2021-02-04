bf4ce856d0cdad73339000e379c6fc6a80b1bd8c
cshoaib05 <choudharyshoaib05@gmail.com>
2021-02-04 22:16:26 +0530

    added

--- Assets/Scripts/LookAt.cs
+++ Assets/Scripts/LookAt.cs
@@ -6,6 +6,8 @@ public class LookAt : MonoBehaviour
 {
     public GameObject Player;
     public Transform[] Positons;
+    public Transform TppCamPos;
+    public GameObject TPPCamera;
 
     private void Start()
     {
@@ -17,5 +19,10 @@ public class LookAt : MonoBehaviour
     {
         Player.GetComponent<Animator>().SetTrigger("run");
         LeanTween.moveLocal(Player, Positons[1].position, 5f);
+        LeanTween.moveLocal(TPPCamera, TppCamPos.position, 1f);
+        LeanTween.rotateLocal(TPPCamera, TppCamPos.rotation.eulerAngles, 1f);
+        TPPCamera.GetComponent<CameraFollow>().enabled = true;
     }
+
+
 }
