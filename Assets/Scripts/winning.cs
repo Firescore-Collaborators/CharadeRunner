7f4e40f06b107b2c825cc82058fd9065476df4bb
cshoaib05 <choudharyshoaib05@gmail.com>
2021-02-08 23:10:51 +0530

    added

---
+++ Assets/Scripts/winning.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class winning : MonoBehaviour
+{
+    public GameObject SkateBoard;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            SkateBoard.SetActive(true);
+            other.gameObject.SetActive(false);
+        }
+    }
+}
