7d650c9cd8a8da9ca13d38a037ec707a020e89f2
cshoaib05 <choudharyshoaib05@gmail.com>
2021-02-12 16:15:33 +0530

    added

--- Assets/Scripts/winning.cs
+++ Assets/Scripts/winning.cs
@@ -7,16 +7,27 @@ public class winning : MonoBehaviour
     public GameObject SkateBoard;
     public GameObject MainCAm;
     public GameObject WinCAm;
+    public GameObject skate;
 
 
     private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.CompareTag("Player"))
         {
-            SkateBoard.SetActive(true);
-            other.gameObject.SetActive(false);
-            MainCAm.SetActive(false);
-            WinCAm.SetActive(true);
+            StartCoroutine(Win(other));
+            
+           
         }
+    }
+
+    IEnumerator Win(Collider other)
+    {
+        other.gameObject.GetComponent<Animator>().SetTrigger("jump");
+        yield return new WaitForSeconds(0.7f);
+        SkateBoard.SetActive(true);
+        other.gameObject.SetActive(false);
+        MainCAm.SetActive(false);
+        WinCAm.SetActive(true);
+        skate.SetActive(false);
     }
 }
