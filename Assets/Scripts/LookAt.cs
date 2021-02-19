1a108c8d904954bf43f7ec9ef6566bc41ceb35f3
cshoaib05 <choudharyshoaib05@gmail.com>
2021-02-19 16:21:55 +0530

    added

--- Assets/Scripts/LookAt.cs
+++ Assets/Scripts/LookAt.cs
@@ -7,6 +7,7 @@ public class LookAt : MonoBehaviour
     public GameObject Player;
     public Transform[] Positons;
     public Transform TppCamPos;
+    public Transform FailPos;
     public GameObject TPPCamera;
     public Animator[] Door;
     public Animator CorrectText;
@@ -41,18 +42,19 @@ public class LookAt : MonoBehaviour
 
     IEnumerator Right()
     {
+
         ConfettiPlay();
         CorrectText.SetTrigger("text");
-        yield return new WaitForSeconds(3f);
-        
-        Player.GetComponent<Animator>().SetTrigger("run");
+        yield return new WaitForSeconds(1f);
         OpenDoor();
+        yield return new WaitForSeconds(2f);
+        Player.GetComponent<Animator>().SetTrigger("run");
         LeanTween.moveLocal(Player, Positons[++count].position, 3f);
         if(AICharacter.Count > 0)
         {
             foreach (GameObject ai in AICharacter)
             {
-                LeanTween.moveLocal(ai, new Vector3(ai.transform.position.x, ai.transform.position.y, Positons[count].position.z), 3f);
+                LeanTween.moveLocal(ai, new Vector3(ai.transform.position.x, ai.transform.position.y, Positons[count].position.z), Random.Range(2.5f, 4f));
             }
 
             AICharacter.RemoveAt(AICharacter.Count - 1);
@@ -79,4 +81,25 @@ public class LookAt : MonoBehaviour
            door.GetComponent<Animator>().SetTrigger("open");
         }
     }
+
+
+    public void WrongAnswer()
+    {
+
+        StartCoroutine(Wrong());
+    }
+
+
+    IEnumerator Wrong()
+    {
+        LeanTween.moveLocal(Player, FailPos.position, 1f);
+        Player.GetComponent<Animator>().SetTrigger("run");
+        LeanTween.moveLocal(TPPCamera, TppCamPos.position, 1f);
+        LeanTween.rotateLocal(TPPCamera, TppCamPos.rotation.eulerAngles, 1f);
+        TPPCamera.GetComponent<CameraFollow>().enabled = true;
+        yield return new WaitForSeconds(1f);
+        Player.GetComponent<Animator>().applyRootMotion = enabled;
+        Player.GetComponent<Animator>().SetTrigger("fall");
+    }
+
 }
